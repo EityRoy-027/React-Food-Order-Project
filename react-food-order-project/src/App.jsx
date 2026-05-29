e06522e2ccbf88158aa955a24dcddd0bd71993ba
eityroy <eityroy027@gmail.com>
2026-05-29 12:48:12 +0600

    react food order project

--- react-food-order-project/src/App.jsx
+++ react-food-order-project/src/App.jsx
@@ -17,11 +17,11 @@ function App() {
 
    <div className="w-full h-full rounded-md bg-gray-300 flex flex-col justify-center items-center gap-4">
 
-   <button className="px-3 py-4 bg-red-600 rounded-md text-white font-bold">Mutton Rezela</button>  
+   <button className="px-4 py-4 bg-red-600 rounded-md text-white font-bold ">Mutton Rezela</button>  
 
-   <button>Burger</button>
+   <button className="px-4 py-4 bg-red-600 rounded-md text-white font-bold "> Chicken Burger</button>
 
-   <button>Chicken Roast</button>
+   <button className="px-5 py-4 bg-red-600 rounded-md text-white font-bold ">Chicken Roast</button>
 
    </div>
 
